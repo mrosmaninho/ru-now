03683c30d1b19772e3f5fe48de5fa2b119a2c6ce
Matheus <mrosmaninhu@outlook.com>
2016-04-27 23:04:53 -0300

    Tentativa de fazer funcionar 01

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
-export default ({}) => {
-  return (
-    <div>
-      <h1>Fila</h1>
-    </div>
-  )
-}
+const DATA = require('./data/menu.js');
+
+var menuField = React.createClass({
+    displayName: 'menuField',
+    getInitialState () {
+        return {};
+    },
+    updateValue (newValue) {
+        this.setState({
+            selectValue: newValue,
+        })
+    },
+    render () {
+        var options = DATA.MENU;
+        return (
+            <div className="sectionMenu">
+                <select ref="citySelect"
+                    autofocus
+                    options={options}
+                    simpleValue
+                    clearable
+                    name="select-city"
+                    value={this.state.selectValue}
+                    onChange={this.updateValue}
+                    searchable
+                    labelKey="name"
+                    valueKey="name"
+                />
+            </div>
+        );
+    }
+});
+module.exports = menuField;
